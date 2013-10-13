888519c9ec6d47145e00d7990e2fb625f7591fc3
Edwin Wargo <edwin@11thhournow.com>
2013-10-13 11:25:57 -0400

    Code for Ben to look at

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require 'faker'
 
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -17,6 +25,19 @@ rand(4..10).times do
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
+    
+  rand(5..12).times do
+    topic = topics.first # getting the first topic here
+    p = u.posts.create(
+      topic: topic,
+      title: Faker::Lorem.words(rand(1..10)).join(" "), 
+      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+    # set the created_at to a time within the past year
+    p.update_attribute(:created_at, Time.now - rand(600..31536000))
+
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
+
+
     rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
@@ -24,11 +45,34 @@ rand(4..10).times do
   end
 end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'info@11thhournow.com', password: 'helloworld', password_confirmation: 'helloworld')
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@11thhournow.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@11thhournow.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@11thhournow.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+end
