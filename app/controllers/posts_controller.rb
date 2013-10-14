fd93a9c4fdfa2f3c5c8d92dcb220320ba2c0f3f6
Edwin Wargo <edwin@11thhournow.com>
2013-10-14 03:34:56 -0400

    Added validation mehtods to post, refactored post views to use a form partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   #end
 
   def show
-    @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
   end
 
   def new
