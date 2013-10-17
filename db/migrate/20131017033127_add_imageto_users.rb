e2ebc13ad3c6382c34871b7bfe83d9390203aa3e
Edwin Wargo <edwin@11thhournow.com>
2013-10-17 00:03:11 -0400

    Image upload code for Tim

---
+++ db/migrate/20131017033127_add_imageto_users.rb
@@ -0,0 +1,7 @@
+class AddImagetoUsers < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014210908) do
+ActiveRecord::Schema.define(:version => 20131017033127) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20131014210908) do
     t.datetime "updated_at",                             :null => false
     t.string   "role"
     t.string   "avatar"
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
