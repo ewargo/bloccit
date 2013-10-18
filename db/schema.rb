f61969fc932202490e41f0b87864bd3c717d00a0
Edwin Wargo <edwin@11thhournow.com>
2013-10-18 14:09:35 -0400

    Added Image to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131017033127) do
+ActiveRecord::Schema.define(:version => 20131017045814) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20131017033127) do
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], :name => "index_posts_on_topic_id"
@@ -62,7 +63,6 @@ ActiveRecord::Schema.define(:version => 20131017033127) do
     t.datetime "updated_at",                             :null => false
     t.string   "role"
     t.string   "avatar"
-    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
