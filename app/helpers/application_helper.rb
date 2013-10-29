1803be79b6f51575262e206911d9272405430c3c
Edwin Wargo <edwin@11thhournow.com>
2013-10-29 16:43:54 -0400

    Created user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,9 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+   def comment_url_helper(comment)
+    post = comment.post
+    topic = post.topic
+    [topic, post, comment]
+    end
 end
